351e3e6012cc60595a9fe7fd5740ba64f4c39958
Extracheesy <78427936+Extracheesy@users.noreply.github.com>
2021-02-21 02:40:18 +0100

    ___

--- load_yfinance_data.py
+++ load_yfinance_data.py
@@ -5,8 +5,10 @@ import yfinance as yf
 from pandas_datareader import data as pdr
 import pandas as pd
 import numpy as np
-from datetime import date, timedelta
 
+from build_DT import *
+import datetime
+from datetime import date, timedelta
 
 def lookup_fn(df, key_row, key_col):
 
@@ -24,28 +26,48 @@ def SaveData(df, filename):
 
     df.to_csv("./data/yfinance_data/" + filename + ".csv")
 
-def getData_5day(ticker):
+def get_Data_5days(ticker):
 
     # We can get data by our choice by giving days bracket
     #start_date= str("2017") + "-" + str("01") + "-" + str("01")
 
     nb_df_row = 0
-    nb_days = 7
+    nb_days = 8
     today = date.today()
 
     start_date = today - timedelta(days=nb_days)
-    data = pdr.get_data_yahoo(ticker, start=start_date, end=today)
+
+    nb_try = 0
+    while True:
+        try:
+            data = pdr.get_data_yahoo(ticker, start=start_date, end=today)
+            break
+        except KeyError:
+            print("I got a KeyError for: ", ticker)
+            nb_try = nb_try + 1
+            if (nb_try > 5):
+                df_empty = pd.DataFrame({"ticker": [ticker],
+                                         "nb_days": [0],
+                                         "deltapercent": [0],
+                                         "deltaprice": [0],
+                                         "DT_results": [0],
+                                         "data_size": [0]})
+                return df_empty
 
     nb_df_row = len(data)
-    print("len rows: ", nb_df_row)
+    #print("len rows: ", nb_df_row)
 
+    if nb_df_row == 8:
+        data = remove_row(data,5)
+        data = remove_row(data,6)
+        data = remove_row(data,7)
     if nb_df_row == 7:
         data = remove_row(data,5)
         data = remove_row(data,6)
     if nb_df_row == 6:
         data = remove_row(data,5)
 
-    print("len rows: ", nb_df_row)
+    #print("len rows: ", nb_df_row)
 
     files = []
     dataname= ticker + "_" + str(today)
@@ -93,53 +115,110 @@ def get_NASDAQ_ticker_list():
     # list all NASDAQ stocks
     url = "ftp://ftp.nasdaqtrader.com/SymbolDirectory/nasdaqlisted.txt"
     df = pd.read_csv(url, sep="|")
-    print(df.head())
-    print(df['Symbol'].head())
-    print(len(df['Symbol']))
+    #print(df.head())
+    #print(df['Symbol'].head())
+    print("nb stocks",len(df['Symbol']))
 
     return df
 
 
-def get_movment_list(df):
+def get_movment_list(stock):
 
-    movementlist = []
+    df_stock_data = get_Data_5days(stock)
+    #print("stock: ",stock)
 
-    for stock in df['Symbol']:
+    if len(df_stock_data) == 1:
+        df_stock_empty = pd.DataFrame({"ticker": [stock],
+                                       "nb_days": [0],
+                                       "deltapercent": [0],
+                                       "deltaprice": [0]})
+        return df_stock_empty
 
-        df_stock_data = getData_5day(stock)
-        print("stock: ",stock)
+    low = float(10000)
+    high = float(0)
 
-        low = float(10000)
-        high = float(0)
+    for low_value in df_stock_data["Low"]:
+        if low_value < low:
+            low = low_value
 
-        for low_value in df_stock_data["Low"]:
-            if low_value < low:
-                low = low_value
+    for high_value in df_stock_data["High"]:
+        if high_value > high:
+            high = high_value
 
-        for high_value in df_stock_data["High"]:
-            if high_value > high:
-                high = high_value
-
+    if low == 0:
+        deltapercent = 0
+    else:
         deltapercent = 100 * (high - low) / low
 
-        len_df = len(df_stock_data)
+    len_df = len(df_stock_data)
 
-        Open = lookup_fn(df_stock_data, 0, "Open")
-        Close = lookup_fn(df_stock_data, len_df - 1, "Close")
+    Open = lookup_fn(df_stock_data, 0, "Open")
+    Close = lookup_fn(df_stock_data, len_df - 1, "Close")
 
+    if Open == 0:
+        deltaprice = 0
+    else:
         deltaprice = 100 * (Close - Open) / Open
 
-        print(stock + " " + str(deltapercent) + " " + str(deltaprice))
-        pair = [stock, deltapercent, deltaprice]
-        movementlist.append(pair)
+    #print(stock + " " + str(deltapercent) + " " + str(deltaprice))
 
-    return movementlist
+    df_movementlist = pd.DataFrame({"ticker": [stock],
+                                    "nb_days": [len_df],
+                                    "deltapercent": [deltapercent],
+                                    "deltaprice": [deltaprice],
+                                    "DT_results": [0],
+                                    "data_size": [0]})
+
+    # pair = [stock, deltapercent, deltaprice]
+
+    return df_movementlist
+
+
+
+
+def get_DT_prediction(stock):
+
+    df_stock_data = getData_5years(stock)
+    stock_DT_predicct = process_decision_tree(df_stock_data,stock)
+
+    return stock_DT_predicct
+
 
 
 def get_data_finance():
 
     df_ticker_list = get_NASDAQ_ticker_list()
+    df_ticker_list.drop([len(df_ticker_list) - 1], axis=0, inplace=True)
 
-    movementlist = get_movment_list(df_ticker_list)
+    SaveData(df_ticker_list, "tickerlist.csv")
+
+    # Calling DataFrame constructor
+    df_movementlist = pd.DataFrame({"ticker": [],
+                                    "nb_days": [],
+                                    "deltapercent": [],
+                                    "deltaprice": [],
+                                    "DT_results": [],
+                                    "data_size": []})
+
+    df_filtered_ticker_list = df_ticker_list[ (df_ticker_list['Test Issue'] == "N")]
+
+    for stock in df_filtered_ticker_list['Symbol']:
+
+            start_stock = datetime.datetime.now()
+
+            df_movementstocklist = get_movment_list(stock)
+
+            DT_result , data_len = process_decision_tree(stock)
+
+            df_movementstocklist["DT_results"] = DT_result
+            df_movementstocklist["data_size"] = round(data_len / 253, 2)
+
+            df_movementlist = df_movementlist.append(df_movementstocklist)
+
+            end_stock = datetime.datetime.now()
+            delta = end_stock - start_stock
+            print("stock:",stock ," time: ",delta)
+
+    SaveData(df_movementlist, "movmentlist.csv")
 
     return df_ticker_list
--- main.py
+++ main.py
@@ -4,8 +4,11 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 import sys
+import os, fnmatch
 import pandas as pd
 import numpy as np
+
+
 
 #from stockstats import StockDataFrame as Sdf
 from load_yfinance_data import *
@@ -15,9 +18,26 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def clear_data_directory():
+
+    listOfFilesToRemove = os.listdir('./data/yfinance_data/')
+    pattern = "*.csv"
+    for entry in listOfFilesToRemove:
+        if fnmatch.fnmatch(entry, pattern):
+            print("remove : ",entry)
+            os.remove("./data/yfinance_data/" + entry)
+
+    listOfFilesToRemove = os.listdir('./data/yfinance_data_DT/')
+    pattern = "*.csv"
+    for entry in listOfFilesToRemove:
+        if fnmatch.fnmatch(entry, pattern):
+            print("remove : ", entry)
+            os.remove("./data/yfinance_data_DT/" + entry)
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+
+    clear_data_directory()
 
     df_data_stock_list = get_data_finance()
 
